31ccf31637fd38be475b4ecf84660b7174ef933f
Vivek <vivek@conneqtiongroup.com>
2026-07-13 13:04:13 +0530

    initialize Express application ,add health check endpoint , add health check endpoint

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./src/app.js";
+
+
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+});
