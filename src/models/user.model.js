94da67b5fe3214bba1587fc5b1fbd31aa12feeaa
Vivek <vivek@conneqtiongroup.com>
2026-07-14 13:45:37 +0530

    installed mongoose and created user model

---
+++ src/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    bio:{
+        type:String,
+    },
+    githuburl:{
+        type:String,
+    },
+    profileImage:{
+        type:String,
+    }, 
+   
+},{timestamps:true});
+
+
+export const User = mongoose.model('User',userSchema);
